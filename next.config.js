7a6d82cd5ed755733dede97ad1af3d4aab328c41
Ghulam Abbas <60650799+ghulamabbas2@users.noreply.github.com>
2023-03-21 00:22:16 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
     DB_URI: "mongodb://localhost:27017/buyitnow",
 
     CLOUD_NAME: "shopitnow-udemy",
-    CLOUDINARY_API_KEY: "357981249752881",
-    CLOUDINARY_API_SECRET: "yejVn_RZb4QFtkI-2mRRyUfzdw8",
+    CLOUDINARY_API_KEY: "",
+    CLOUDINARY_API_SECRET: "",
 
     NEXTAUTH_SECRET: "codingwithabbas",
   },
