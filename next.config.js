e72b5d6aaf03a47b1837d02e3108f4f39e4cc519
Ghulam Abbas <ghulamabbas258@gmail.com>
2023-04-18 20:08:39 +0500

    added stripe and created checkout session

--- next.config.js
+++ next.config.js
@@ -8,13 +8,11 @@ const nextConfig = {
     DB_URI: "mongodb://localhost:27017/buyitnow",
 
     CLOUD_NAME: "shopitnow-udemy",
-    CLOUDINARY_API_KEY: "357981249752881",
-    CLOUDINARY_API_SECRET: "yejVn_RZb4QFtkI-2mRRyUfzdw8",
+    CLOUDINARY_API_KEY: "",
+    CLOUDINARY_API_SECRET: "",
 
-    STRIPE_PUBLIC_KEY:
-      "pk_test_51HgmC8AlHMiRMt8EK5BPmz6yWtXxNfd7SUyl3v4Eznw5laesRGGQiWM0vrVWBKzGm3dZmnO9WUBRr9JFJL4L9OlD0068rUgWEx",
-    STRIPE_PRIVATE_KEY:
-      "sk_test_51HgmC8AlHMiRMt8EQq51fHlYRq3lsZOIQE4LaVdj6Zi9hEoySb0s6uAPQVjQNtZf77VaL3pNQTd0XurtdMNNdH3w005Kq4agAj",
+    STRIPE_PUBLIC_KEY: "",
+    STRIPE_PRIVATE_KEY: "",
 
     NEXTAUTH_SECRET: "codingwithabbas",
   },
